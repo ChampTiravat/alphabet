c2db716c83fb3d5dc83465354dfd7bee9cea7213
champtiravat <tiravat2016@gmail.com>
2019-12-29 15:57:01 +0700

    :hammer: Refactored

--- main.go
+++ main.go
@@ -4,23 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
-const (
-	serverPort string = ":9000"
-)
+type Response struct {
+	Data string `json: data`
+}
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		fmt.Fprintf(w, "ok")
-		return
+// procedural-oriented programming langauge
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	rawRes := &Response{Data: "hello, World!"}
+	data, err := json.Marshal(&rawRes)
+	if err != nil {
+		log.Println(err)
 	}
-	fmt.Fprintf(w, "Method Not Allowed")
+
+	if r.Method == "GET" {
+		fmt.Fprintf(w, string(data))
+	} else {
+		fmt.Fprintf(w, "method not allowed")
+	}
 	return
 }
 
 func main() {
-	log.Printf("API Server Started at http://0.0.0.0:%s\n", serverPort)
-	http.HandleFunc("/", rootHandler)
-	http.ListenAndServe(serverPort, nil)
+	http.HandleFunc("/", handleIndex)
+	http.ListenAndServe(":9000", nil)
 }
